4269840678b6827670c4233c43cc5eae88889f97
Wade Shen <swadey@mit.edu>
2017-07-08 13:28:01 -0400

    - update

--- utils.js
+++ utils.js
@@ -3,23 +3,28 @@
 // ------------------------------------------------------------------------------------------------------------------
 const draft   = require('draftlog').into(console);
 const c       = require('chalk');
-const sprintf = require('sprintf').sprintf;
+const sprintf = require('sprintf-js').sprintf;
 
-const spinner_state = [ "-", "\\", "|", "/" ];
+const spinner_state = [ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" ];
 
 // ------------------------------------------------------------------------------------------------------------------
 // Utilities
 // ------------------------------------------------------------------------------------------------------------------
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function elapsed(t0) {
   let dt = process.hrtime();
   return (dt[0] - t0[0]) + (dt[1] - t0[1]) / 1e9;
 }
 
-function format_tag(s, { color : c.red, pad : 15 } = {}) {
-  return sprintf(`%${pad}d`, color(s));
+function format_tag(s, { color = c.red, pad = 15 } = {}) {
+  //return sprintf(`%s`, color(s) + " ".repeat(Math.max(0, pad - s.length)));
+  return sprintf(`%-${pad}s`, color(s));
 }
 
-function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = (g) => "" } = {}) {
+function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = () => "" } = {}) {
   function paint_bar(N, width) {
     return c.dim('[') + c.blue('=').repeat(N) + ' '.repeat(width - N) + c.dim('] ');
   }
@@ -39,7 +44,7 @@ function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = 
     u_text          : user_text,
     status_text     : function (rate, b_rate) {
       return sprintf("%15d complete (%d items/sec [total], %d items/sec [current interval]%s)",
-                     this.N, rate, b_rate, this.u_text(this));
+                     this.N, rate, b_rate, this.u_text());
     },
     pulse           : function() {
       this.N += 1;
@@ -54,8 +59,8 @@ function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = 
   };
 }
 
-function make_spinner(tag, { interval = 10000, user_text = (g) => "" } = {}) {
-  let log = console.draft(format_tag(tag), c.blue("[" + spinner_state[0] + "]") + " starting...");
+function make_spinner(tag, { interval = 10000, user_text = () => "" } = {}) {
+  let log = console.draft(format_tag(tag), c.dim("[") + c.blue(spinner_state[0]) + c.dim("]") + " starting...");
   return {
     tag             : tag,
     log             : log,
@@ -66,7 +71,7 @@ function make_spinner(tag, { interval = 10000, user_text = (g) => "" } = {}) {
     u_text          : user_text,
     status_text     : function (rate, b_rate) {
       return sprintf("%15d complete (%d items/sec [total], %d items/sec [current interval]%s)",
-                     this.N, rate, b_rate, this.u_text(this));
+                     this.N, rate, b_rate, this.u_text());
     },
     pulse           : function() {
       this.N += 1;
@@ -74,7 +79,7 @@ function make_spinner(tag, { interval = 10000, user_text = (g) => "" } = {}) {
         let elapsed_time = elapsed(this.t0);
         let batch_time   = elapsed(this.bt0);
         let fill         = Math.min(Math.round((this.N / this.limit) * this.width), this.limit);
-        this.log(format_tag(this.tag) + c.blue(" [" + spinner_state[(this.N / this.update_interval) % spinner_state.length] + "]") +
+        this.log(format_tag(this.tag) + c.dim(" [") + c.blue(spinner_state[(this.N / this.update_interval) % spinner_state.length]) + c.dim("]") +
                  this.status_text(this.N / elapsed_time, this.update_interval / batch_time));
         this.bt0 = process.hrtime();
       }
@@ -88,3 +93,4 @@ function make_spinner(tag, { interval = 10000, user_text = (g) => "" } = {}) {
 exports.sprintf      = sprintf;
 exports.make_gauge   = make_gauge;
 exports.make_spinner = make_spinner;
+exports.sleep        = sleep;
--- utils.test.js
+++ utils.test.js
@@ -1,6 +1,6 @@
 const utils = require('./utils.js');
 
-test("basic gauges",() => {
+test("basic gauges", async () => {
   let gauge = utils.make_gauge("[test]", { limit : 10 });
   expect(gauge.update_interval).toBe(1);
   expect(gauge.limit).toBe(10);
@@ -9,4 +9,11 @@ test("basic gauges",() => {
     gauge.pulse();
     expect(gauge.N).toBe(i + 1);
   }
+
+  let spinner = utils.make_spinner("[test]", { interval : 1 });
+  for (let i = 0; i < 10; i++) {
+    spinner.pulse();
+    await utils.sleep(100);
+  }
+  
 });

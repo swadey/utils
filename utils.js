1300b97da0a0433503f5b4e10f13606da97d201d
Wade Shen <swadey@mit.edu>
2017-07-09 14:48:15 -0400

    - theme colors

--- utils.js
+++ utils.js
@@ -7,6 +7,14 @@ const sprintf = require('sprintf-js').sprintf;
 const docopt  = require('docopt');
 
 const spinner_state = [ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" ];
+const default_theme = {
+  complete_color: c.green,
+  progress_color: c.yellow,
+  tag_color: (s) => c.red(c.bold(s)),
+  bracket_color: c.dim,
+  time_color: c.cyan,
+  user_color: c.white,
+};
 
 // ------------------------------------------------------------------------------------------------------------------
 // Utilities
@@ -20,18 +28,18 @@ function elapsed(t0) {
   return (dt[0] - t0[0]) + (dt[1] - t0[1]) / 1e9;
 }
 
-function format_tag(s, { color = c.red, pad = 15 } = {}) {
+function format_tag(s, color, { pad = 15 } = {}) {
   return sprintf(`%s`, color(s) + " ".repeat(Math.max(0, pad - s.length)));
 }
 
-function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = () => "" } = {}) {
-  function paint_bar(N, width, { color = c.yellow } = {}) {
-    return c.dim('[') + color('=').repeat(N) + ' '.repeat(width - N) + c.dim('] ');
+function make_gauge(tag, { limit = 0, interval = 10000, width = 30, theme = default_theme, user_text = () => "" } = {}) {
+  function paint_bar(N, width, color) {
+    return theme.bracket_color('[') + color('=').repeat(N) + ' '.repeat(width - N) + theme.bracket_color('] ');
   }
 
   let real_width = Math.min(width, limit);
   let real_interval = Math.min(interval, Math.ceil(limit / 10));
-  let log = console.draft(format_tag(tag) + " " + paint_bar(0, real_width) + "starting...");
+  let log = console.draft(format_tag(tag, theme.tag_color) + " " + paint_bar(0, real_width, theme.progress_color) + "starting...");
   return {
     tag             : tag,
     log             : log,
@@ -43,12 +51,12 @@ function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = 
     width           : real_width,
     u_text          : user_text,
     status_text     : function (rate, b_rate) {
-      return sprintf("%12d complete (%d items/sec [total], %d items/sec [current interval]%s)",
-                     this.N, rate, b_rate, this.u_text());
+      return sprintf("%12d complete (" + theme.time_color("%d items/sec [total], %d items/sec [current interval]") + "%s" + ")",
+                     this.N, rate, b_rate, theme.user_color(this.u_text()));
     },
     final_status    : function(elapsed_time, rate) {
-      return sprintf("%12d " + c.green("[complete]") + " (total time: %d [secs], %d items/sec [total]%s)",
-                     this.N, elapsed_time, rate, this.u_text());
+      return sprintf("%12d " + theme.complete_color("[complete]") + " (" + theme.time_color("total time: %d [secs], %d items/sec [total]") + "%s" + ")",
+                     this.N, elapsed_time, rate, theme.user_color(this.u_text()));
     },
     pulse           : function() {
       this.N += 1;
@@ -56,24 +64,26 @@ function make_gauge(tag, { limit = 0, interval = 10000, width = 30, user_text = 
         let elapsed_time = elapsed(this.t0);
         let batch_time   = elapsed(this.bt0);
         let fill         = Math.min(Math.round((this.N / this.limit) * this.width), this.width);
-        this.log(format_tag(this.tag) + " " + paint_bar(fill, this.width) + this.status_text(this.N / elapsed_time, this.update_interval / batch_time));
+        this.log(format_tag(this.tag, theme.tag_color) + " " + paint_bar(fill, this.width, theme.progress_color) +
+                 this.status_text(this.N / elapsed_time, this.update_interval / batch_time));
         this.bt0 = process.hrtime();
       }
     },
     complete        : function() {
       let elapsed_time = elapsed(this.t0);
       let fill         = Math.min(Math.round((this.N / this.limit) * this.width), this.width);
-      this.log(format_tag(this.tag) + " " + paint_bar(fill, this.width, { color : c.green }) + this.final_status(elapsed_time, this.N / elapsed_time));
+      this.log(format_tag(this.tag, theme.tag_color) + " " + paint_bar(fill, this.width, theme.complete_color) +
+               this.final_status(elapsed_time, this.N / elapsed_time));
     }
   };
 }
 
-function spin(i) {
-  return c.dim("[") + c.yellow(spinner_state[i]) + c.dim("]");
+function spin(i, bcolor, color) {
+  return bcolor("[") + color(spinner_state[i]) + bcolor("]");
 }
 
-function make_counter(tag, { interval = 10000, user_text = () => "" } = {}) {
-  let log = console.draft(format_tag(tag) + " " + spin(0) + " starting...");
+function make_counter(tag, { interval = 10000, theme = default_theme, user_text = () => "" } = {}) {
+  let log = console.draft(format_tag(tag, theme.tag_color) + " " + spin(0, theme.bracket_color, theme.progress_color) + " starting...");
   return {
     tag             : tag,
     log             : log,
@@ -84,12 +94,12 @@ function make_counter(tag, { interval = 10000, user_text = () => "" } = {}) {
     update_interval : interval,
     u_text          : user_text,
     status_text     : function (rate, b_rate) {
-      return sprintf("%12d complete (%d items/sec [total], %d items/sec [current interval]%s)",
-                     this.N, rate, b_rate, this.u_text());
+      return sprintf("%12d complete (" + theme.time_color("%d items/sec [total], %d items/sec [current interval]") + "%s" + ")",
+                     this.N, rate, b_rate, theme.user_color(this.u_text()));
     },
     final_status    : function(elapsed_time, rate) {
-      return sprintf("%12d " + c.green("[complete]") + " (total time: %d secs, %d items/sec%s)",
-                     this.N, elapsed_time, rate, this.u_text());
+      return sprintf("%12d " + theme.complete_color("[complete]") + " (" + theme.time_color("total time: %d secs, %d items/sec") + "%s" + ")",
+                     this.N, elapsed_time, rate, theme.user_color(this.u_text()));
     },
     pulse           : function() {
       this.N += 1;
@@ -98,23 +108,26 @@ function make_counter(tag, { interval = 10000, user_text = () => "" } = {}) {
         let batch_time   = elapsed(this.bt0);
         let fill         = Math.min(Math.round((this.N / this.limit) * this.width), this.limit);
         this.state = (this.state + 1) % spinner_state.length;
-        this.log(format_tag(this.tag) + " " + spin((this.N / this.update_interval) % spinner_state.length) + " " +
+        this.log(format_tag(this.tag, theme.tag_color) + " " +
+                 spin((this.N / this.update_interval) % spinner_state.length, theme.bracket_color, theme.progress_color) + " " +
                  this.status_text(this.N / elapsed_time, this.update_interval / batch_time));
         this.bt0 = process.hrtime();
       }
     },
     complete        : function() {
       let elapsed_time = elapsed(this.t0);
-      this.log(format_tag(this.tag) + c.dim(" [") + c.green("✔") + c.dim("] ") + this.final_status(elapsed_time, this.N / elapsed_time));
+      this.log(format_tag(this.tag, theme.tag_color) +
+               theme.bracket_color(" [") + theme.complete_color("✔") + theme.bracket_color("] ") +
+               this.final_status(elapsed_time, this.N / elapsed_time));
     }
   };
 }
 
-function make_spinner(tag, text, { interval = 10000 } = {}) {
+function make_spinner(tag, text, { interval = 10000, theme = default_theme } = {}) {
   var state = 0;
-  let log = console.draft(format_tag(tag) + " " + spin(state) + " " + text);
+  let log = console.draft(format_tag(tag, theme.tag_color) + " " + spin(state, theme.bracket_color, theme.progress_color) + " " + text);
   let id  = setInterval(() => {
-    log(format_tag(tag) + " " + spin(state) + " " + text + ` ${state}`);
+    log(format_tag(tag, theme.tag_color) + " " + spin(state, theme.bracket_color, theme.progress_color) + " " + text);
     state = (state + 1) % spinter_state.length;
   }, interval);
 
@@ -124,7 +137,9 @@ function make_spinner(tag, text, { interval = 10000 } = {}) {
     t0       : process.hrtime(),
     complete : function() {
       clearInterval(id);
-      this.log(format_tag(this.tag) + c.dim(" [") + c.green("✔") + c.dim("] ") + text + c.green(" [complete]") + ` (${elapsed(this.t0)} seconds)`);
+      this.log(format_tag(this.tag, theme.tag_color) +
+               theme.bracket_color(" [") + theme.complete_color("✔") + theme.complete_color("] ") +
+               text + theme.complete_color(" [complete]") + " (" + theme.time_color(`${elapsed(this.t0)} seconds`) + ")");
     }
   };
 }
